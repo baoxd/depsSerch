62742d6472c95fef89ca8a56bc872bb51b334548
baoxd <1459815090@qq.com>
2018-06-15 15:54:30 +0800

    add file utils

--- utils/file.js
+++ utils/file.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
 /**
  * 读物文件夹
  * @param {*} path 
  */
-exports.readDir = function(path) {
+exports.readDir = function(path, cb) {
+    try{
+        const files = fs.readdirSync(path);
+        cb && cb(files);
+    } catch(e) {
+        console.log('文件夹读取错误');
+        console.log(e);
+    }
+}
 
+exports.readFile = function(file, cb) {
+    try{
+        const code = fs.readFileSync(file, 'utf-8');
+        cb && cb(code);
+    } catch(e) {
+        console.log('文件读取错误');
+        console.log(e);
+    }
 }
