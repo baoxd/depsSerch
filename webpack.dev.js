65f354c9dccf16b7ab89fbb7a9aee9fdd197ccca
baoxd <1459815090@qq.com>
2018-06-20 19:31:22 +0800

    update import regexp

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,10 @@
 const path = require('path');
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var APP_PATH = path.resolve(__dirname);;
 
 module.exports = function() {
     return {
         alias: {
-            module: path.resolve(APP_PATH, 'module'),
-            service: path.resolve(APP_PATH, 'service'),
-            component: path.resolve(APP_PATH, 'component'),
-            page: path.resolve(APP_PATH, 'page'),
-            reactPage: path.resolve(APP_PATH, 'reactPage'),
-            reactComponent: path.resolve(APP_PATH, 'reactComponent'),
-            reactModule: path.resolve(APP_PATH, 'reactModule'),
-            reactTools: path.resolve(APP_PATH, 'reactTools'),
-            node_modules: path.resolve(ROOT_PATH, 'node_modules'),
+            app: APP_PATH
         },
 
     };
