73c002d025ddd5d0cc99e10ab805b66028e991e6
baoxd <1459815090@qq.com>
2018-06-22 16:07:33 +0800

    添加入口文件路径参数

--- bin/config.js
+++ bin/config.js
@@ -15,6 +15,10 @@ module.exports = function(yargs) {
                     alias: 's',
                     describe: '信息显示类型, a: 显示所有；o: 是显示没有被依赖的文件',
                     type: 'string'
+                }).option('input', {
+                    alias: 'i',
+                    describe: '项目入口文件文件夹路径',
+                    type: 'string'
                 });
         })
 		.help('h')
